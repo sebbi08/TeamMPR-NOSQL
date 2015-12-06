b8be0cc00b293b73b96041bbfa7e524409c83871
Herbe_000 <Herbe_000@Michael.HomeAD.local>
2015-12-07 00:38:05 +0100

    added some routes added actual arangedb dump

--- app/app.js
+++ app/app.js
@@ -9,7 +9,8 @@ app.use(bodyParser.json());
 
 var arangojs = require('arangojs');
 var db = arangojs();
-var footballDB = db.useDatabase('FootballWebapplication');
+var footballDB = db.useDatabase('FBWebApp');
+
 
 app.get("/clubs", function (req, res) {
     var clubs = footballDB.collection('Club');
@@ -21,17 +22,55 @@ app.get("/clubs", function (req, res) {
 });
 
 app.get("/players", function (req, res) {
-    var clubs = footballDB.collection('Player');
-    clubs.all().then(function (curser) {
+    var players = footballDB.collection('Player');
+    players.all().then(function (curser) {
         curser.all().then(function (result) {
             res.json(result);
         });
     });
 });
 
+//Todo maybe delete res.send ... is this method needed??
 app.post('/players', function (req, res) {
-    console.log(req.body.name);
-    res.send(req.body.name);
+    var newPlayer = {"name": req.body.name,"surName": req.body.surName,"scoredGoals":"","dateOfBirth": req.body.dateOfBirth};
+    var players = footballDB.collection('Player');
+    
+    players.save(newPlayer, function (err, result) {
+        if (err) {
+            console.log('error: %j', err.message); 
+        } else {
+            //console.log('result: %j', result._id);
+            res.send(result._id);
+        }
+    });
 });
+
+app.get("/matches", function (req, res) {
+    var matches = footballDB.collection('Match');
+    matches.all().then(function (curser) {
+        curser.all().then(function (result) {
+            res.json(result);
+        });
+    });
+});
+
+app.get("/leagues", function (req, res) {
+    var leagues = footballDB.collection('League');
+    leagues.all().then(function (curser) {
+        curser.all().then(function (result) {
+            res.json(result);
+        });
+    });
+});
+
+app.get("/trainers", function (req, res) {
+    var trainers = footballDB.collection('Trainer');
+    trainers.all().then(function (curser) {
+        curser.all().then(function (result) {
+            res.json(result);
+        });
+    });
+});
+
 
 module.exports = app;
