0813bd4b6cbd73bf0ec8d6c4dab6f89f543ef993
Mahr Sebastian <sebbi_mahr@gmx.de>
2015-12-11 23:45:40 +0100

    Load Data implemented unless the create match methode
    player json convertetd to UTF-8
    
    database connection is stored in Settings.js

--- documentation/loadDatabase.js
+++ documentation/loadDatabase.js
@@ -1,103 +1,167 @@
-var db = require('monk')('localhost/FBWebApp');
+var settings = require("../settings");
 
-systemdb.dropDatabase("FBWebApp").then(function(){
-    createDatabase();
-},function(err){
-    console.log(err);
-    createDatabase();
+var mongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://' + settings.ip + ':' + settings.port + '/' + settings.name + '';
+mongoClient.connect(url, function (err, db) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    var promises = [];
+    promises.push(db.collection('Club').drop());
+    promises.push(db.collection('Player').drop());
+    promises.push(db.collection('Match').drop());
+    promises.push(db.collection('League').drop());
+
+    Promise.all(promises).then(createDatabase());
 });
 
 
-function createDatabase(){
-    var db = require('arangojs')();
-    db.createDatabase("FBWebApp").then(function(info){
-        var footballDB = db.useDatabase('FBWebApp');
-        var club = footballDB.collection("Club");
-        var league = footballDB.collection("League");
-        var match = footballDB.collection("Match");
-        var player = footballDB.collection("Player");
-        var clubsInLeague = footballDB.edgeCollection("clubsInLeague");
-        var creationsPromises = [];
+function createDatabase() {
+    var db = require('monk')(settings.ip + ':' + settings.port + '/' + settings.name);
+    var clubCollection = db.get("Club");
+    var playerCollection = db.get("Player");
+    var matchCollection = db.get("Match");
+    var leagueCollection = db.get("League");
 
 
-        var leaguePromis = league.create();
-        creationsPromises.push(leaguePromis);
-        leaguePromis.then(function(){
-            creationsPromises.push(loadLeagues(league));
+    //First of creating LeaLeagues becaus they got no dependencys
+    loadLeagues(leagueCollection).then(function () {
+
+        //getting the id's for the two leagues
+        var ersteBundesligaId, zweiteBundesligaId;
+        leagueCollection.find({}, {stream: true}).each(function (doc) {
+            if (doc.name.contains("1")) {
+                ersteBundesligaId = doc._id;
+            } else {
+                zweiteBundesligaId = doc._id;
+            }
+        }).success(function () {
+
+            //after getting the ids loading the clubs from json and connecting them to one of the two leagues
+            loadClubs(clubCollection, [ersteBundesligaId, zweiteBundesligaId]).then(function () {
+
+                //load all clubs to use the id's in the loadPlayer Method
+                clubCollection.find().then(function (clubs) {
+
+                    // load players from json and connecting them to the Club via ID
+                    loadPlayers(playerCollection, clubs).then(function () {
+
+                        //TODO call CreateMatches with parameters i dont know
+                        //createMatchs().then(function () {
+                        //});
+                        db.close();
+                        console.log("Database Created");
+
+                    });
+                });
+            });
         });
-
-        var clubPromis = club.create();
-        creationsPromises.push(clubPromis);
-        clubPromis.then(function(){
-            creationsPromises.push(loadClubs(club,league));
-        });
-
-        var playerPromis = player.create();
-        creationsPromises.push(playerPromis);
-        playerPromis.then(function(){
-            creationsPromises.push(loadPlayers(player,club));
-        });
-
-        Promise.all(creationsPromises).then(function(){
-            match.create().then(createMatchs(match,league,club));
-        });
-
-
-
-    },function(err){
-        console.log(err)
     });
-
-
 }
 
 
 function loadLeagues(leagueCollection) {
-    leagueCollection.save({name : "1. Bundesliga"},function (err, result) {
-        if(err){
+    var promises = [];
+    var promise1 = leagueCollection.insert({name: "1. Bundesliga"});
+    promise1.then(function (result, err) {
+        if (err) {
             console.log(err);
         }
     });
 
-    leagueCollection.save({name : "2. Bundesliga"},function (err, result) {
-        if(err){
+    promises.push(promise1);
+
+    var promise2 = leagueCollection.insert({name: "2. Bundesliga"});
+    promise2.then(function (result, err) {
+        if (err) {
             console.log(err);
         }
     });
+
+    promises.push(promise2);
+
+    return Promise.all(promises);
 }
 
-function loadClubs(clubCollection,leagueCollection,matchCollection){
+function loadClubs(clubCollection, leagueId) {
     var clubJson = require("./clubs.json");
-    var finishCounter = 0;
-    clubJson.forEach(function(club){
-        var leagueId = club.wettbewerbsId;
-        var league = leagueId.substring(leagueId.length - 1);
-        league = parseInt(league);
-        if(league < 3){
-            finishCounter++;
-            var databaseClub = {
-                name : club.langschreibweise,
-                shortName : club.dreiBuchstabenCode,
-                league : league,
-                points:0,
-                scoredGoals:0,
-                receivedGoals:0
-            };
+    var clubImages = require("./clubLogos.json");
+    var clubPromises = [];
 
-            clubCollection.save(databaseClub,function (err, result) {
-                if(err){
+
+    clubJson.forEach(function (club) {
+
+        //convert wettbewerbsId to league number 1 = 1. Bundesliga , 2 = 2. Bundesliga
+        var wettbewerbsId = club.wettbewerbsId;
+        var league = wettbewerbsId.substring(wettbewerbsId.length - 1);
+        league = parseInt(league);
+
+        //if club is in league 1 or 2 creat him connect him to the league and save him to the database
+        if (league < 3) {
+
+            var databaseClub = {
+                name: club.langschreibweise,
+                shortName: club.dreiBuchstabenCode,
+                league: leagueId[league - 1],
+                points: 0,
+                scoredGoals: 0,
+                receivedGoals: 0,
+                imageUrl: "http:" + clubImages[club.id].logo40
+            };
+            var promis = clubCollection.insert(databaseClub).then(function (result, err) {
+                if (err) {
                     console.log(err);
                 }
             });
+            clubPromises.push(promis);
         }
+    });
 
-    })
+    return Promise.all(clubPromises)
 }
 
-function loadPlayers(playerCollection,clubCollection){
+function loadPlayers(playerCollection, clubs) {
 
+    var playerJson = require("./players.json");
+
+    var promises = [];
+
+    playerJson.forEach(function (playerJson) {
+
+        //if the play is in no club ignore him
+        if (playerJson.club.length != 0) {
+
+
+            for (var i = 0; i < clubs.length; i++) {
+                var club = clubs[i];
+
+                //if the player is in the current club creat him, connect him to the club wia its id and write the Object to the database
+                if (club.name === playerJson.club) {
+                    var age = Math.floor(Math.random() * (36 - 16) + 16);
+
+                    var player = {
+                        name: playerJson.name.split(" ")[0],
+                        age: age,
+                        scoredGoals: 0,
+                        sureName: playerJson.name.split(" ")[1],
+                        clubId: club._id
+                    };
+
+                    promises.push(playerCollection.insert(player))
+                }
+            }
+        }
+    });
+
+    return Promise.all(promises)
 }
 
-function createMatchs(matchCollection,leagueCollection){
- // macht michael
+function createMatchs(matchCollection, leagueCollection) {
+    // macht michael
 }
+
+
+String.prototype.contains = function (it) {
+    return this.indexOf(it) != -1;
+};
---
+++ settings.js
@@ -0,0 +1,7 @@
+// file to add settings like database connection ip and pw
+
+module.exports = {
+    ip : "localhost",
+    name : "FBWebApp",
+    port : 27017
+};
