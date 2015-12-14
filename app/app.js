275acc748e53b8285803cfa5df2e24b73753fdd4
Sebastian Mahr <sebbi_mahr@gmx.de>
2015-12-14 08:10:42 +0100

    Fixed Version Depenedencys
    added get methodes for all entetys

--- app/app.js
+++ app/app.js
@@ -1,78 +1,82 @@
-
 var express = require("express");
 var app = express();
 
 var bodyParser = require('body-parser');
+var settings = require('../settings');
 
-var db = require('arangojs')();
-var footballDB = db.useDatabase('FBWebApp');
 
+var db = require('monk')(settings.ip + ':' + settings.port + '/' + settings.name);
 
 
 app.use(bodyParser.json());
 
-app.all("/*",function(req,res,next){
+app.all("/*", function (req, res, next) {
+    //TODO Calculate Matches that soud have been played
     //calculateMatches();
+    console.log("Call to " + req.path);
     next()
 });
 
 app.get("/clubs", function (req, res) {
-    var clubs = footballDB.collection('Club');
-    clubs.all().then(function (curser) {
-        curser.all().then(function (result) {
-            res.json(result);
-        });
-    });
-});
-
-app.get("/players", function (req, res) {
-    var players = footballDB.collection('Player');
-    players.all().then(function (curser) {
-        curser.all().then(function (result) {
-            res.json(result);
-        });
-    });
-});
-
-//Todo maybe delete res.send ... is this method needed??
-app.post('/players', function (req, res) {
-    var newPlayer = {"name": req.body.name,"surName": req.body.surName,"scoredGoals":"","dateOfBirth": req.body.dateOfBirth};
-    var players = footballDB.collection('Player');
-    
-    players.save(newPlayer, function (err, result) {
+    var collection = db.get("Club");
+    collection.find({}, {}, function (err, data) {
         if (err) {
-            console.log('error: %j', err.message); 
+            console.log(err);
         } else {
-            //console.log('result: %j', result._id);
-            res.send(result._id);
+            res.json(data);
         }
     });
 });
 
+app.get("/players", function (req, res) {
+    var collection = db.get("Player");
+    collection.find({}, {}, function (err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(data);
+        }
+    });
+});
+
+//Todo maybe delete res.send ... is this method needed??
+//Answer TODO no is not needet but normaly u give a response if the opersation was succesfull or not and if not provide an error
+/*
+ app.post('/players', function (req, res) {
+ var newPlayer = {"name": req.body.name,"surName": req.body.surName,"scoredGoals":"","dateOfBirth": req.body.dateOfBirth};
+ var players = footballDB.collection('Player');
+
+ players.save(newPlayer, function (err, result) {
+ if (err) {
+ console.log('error: %j', err.message);
+ } else {
+ //console.log('result: %j', result._id);
+ res.send(result._id);
+ }
+ });
+ });
+ */
+
+
 app.get("/matches", function (req, res) {
-    var matches = footballDB.collection('Match');
-    matches.all().then(function (curser) {
-        curser.all().then(function (result) {
-            res.json(result);
-        });
+    var collection = db.get("Matche");
+    collection.find({}, {}, function (err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(data);
+        }
     });
 });
 
 app.get("/leagues", function (req, res) {
-    var leagues = footballDB.collection('League');
-    leagues.all().then(function (curser) {
-        curser.all().then(function (result) {
-            res.json(result);
-        });
-    });
-});
-
-app.get("/trainers", function (req, res) {
-    var trainers = footballDB.collection('Trainer');
-    trainers.all().then(function (curser) {
-        curser.all().then(function (result) {
-            res.json(result);
-        });
+    var collection = db.get("League");
+    collection.find({}, {}, function (err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(data);
+        }
     });
 });
 
--- server.js
+++ server.js
@@ -11,8 +11,10 @@ var express = require("express");
 //Static Routes for Public folder
 app.use('/', express.static(__dirname + '/public'));
 
-var port = 8080;
+var port = 8080 || process.env.port;
+var ip = "localhost" || process.env.ip;
 app.set('port', port);
+app.set('ip',ip);
 //app.use(bodyParser.json());
 
 
@@ -26,6 +28,6 @@ var server = http.createServer(app);
  * Listen on provided port, on all network interfaces.
  */
 app.listen(process.env.port || app.get('port'), function(){
-    console.log("Express server listening on port %d in %s mode",
-        app.get('port') || process.env.port, app.settings.env);
+    console.log("Express server listening on port %d in %s mode \n http://%s:%d",
+        app.get('port'), app.settings.env,app.get('ip'),app.get('port'));
 });
