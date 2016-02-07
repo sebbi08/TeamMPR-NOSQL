05624abfbec124551700a01583085ab24c6d4e75
Herbe_000 <Herbe_000@SurfacePro.home>
2016-02-08 00:40:10 +0100

    added calculateMatches() with several helper methods. Still doesn't work correctly. Maybe a problem with async execution?

--- SeasonCalculationService.js
+++ SeasonCalculationService.js
@@ -1,7 +1,146 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 
+var settings = require('./settings');
+var db = require('monk')(settings.ip + ':' + settings.port + '/' + settings.name);
+
+exports.calculateMatches = function () {
+    //DEBUG: console.log("calculateMatches called");
+    var promises = [];
+    var matchCollection = db.get("Match");
+    var actualDate = new Date();
+
+    matchCollection.find().then(function (matches) {
+        //DEBUG: console.log("matchCollection find() called");
+        matches.forEach(function (match) {
+            if (match.date <= actualDate && match.played === false) {
+                var homeGoals = Math.floor(Math.random() * (5 - 0));
+                var guestGoals = Math.floor(Math.random() * (5 - 0));
+
+                addGoals(match, homeGoals, guestGoals).then(function (match, homeGoals, guestGoals) {
+                    addPoints(match, homeGoals, guestGoals).then(function (match) {
+                        match.played = true;
+                        //Er scheint hier nen Problem zu haben da er es nicht in die DB schreibt.
+                        promises.push(matchCollection.updateById(match._id, match, function (err, doc) {
+                            if (err) {
+                                console.log(err);
+                                return;
+                            }
+                        }));
+                    });
+                });
+
+
+
+                //console.log(match.homeGoals + ":" + match.guestGoals);
+
+
+
+                // DEBUG: console.log("date checked" + match.date + " --> " + actualDate + " --> " + homeGoals + " --> " + guestGoals);
+
+            }
+        });
+        Promise.all(promises);
+        //DEBUG: console.log("promise done");
+    });
+};
+
+function addPoints(match, homeGoals, guestGoals) {
+    //console.log("addPoints called");
+    var clubCollection = db.get("Club");
+    var homeClub;
+    var guestClub;
+
+    //console.log(match.homeClub);
+
+    clubCollection.findById(match.homeClub, {}, function (err, club) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        homeClub = club;
+
+        clubCollection.findById(match.guestClub, {}, function (err, club) {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            guestClub = club;
+//            console.log(homeClub.points);
+//            console.log("Points showed");
+
+            if (homeGoals - guestGoals > 0) {
+
+                clubCollection.updateById(match.homeClub, {
+                    points: match.homeClub.points + 3,
+                    scoredGoals: match.homeClub.scoredGoals + homeGoals,
+                    receivedGoals: match.homeClub.receivedGoals + guestGoals,
+                    won: match.homeClub.won + 1,
+                });
+                clubCollection.updateById(match.guestClub, {
+                    scoredGoals: match.guestClub.scoredGoals + guestGoals,
+                    receivedGoals: match.guestClub.receivedGoals + homeGoals,
+                    lost: match.guestClub.lost + 1,
+                });
+            } else if (homeGoals - guestGoals < 0) {
+                clubCollection.updateById(match.homeClub, {
+                    scoredGoals: match.homeClub.scoredGoals + homeGoals,
+                    receivedGoals: match.homeClub.receivedGoals + guestGoals,
+                });
+                clubCollection.updateById(match.guestClub, {
+                    points: match.homeClub.points + 3,
+                    scoredGoals: match.guestClub.scoredGoals + homeGoals,
+                    receivedGoals: match.guestClub.receivedGoals + guestGoals,
+                    won: match.guestClub.lost + 1,
+                });
+            } else {
+                clubCollection.updateById(match.homeClub, {
+                    points: match.homeClub.points + 1,
+                    scoredGoals: match.homeClub.scoredGoals + homeGoals,
+                    receivedGoals: match.homeClub.receivedGoals + guestGoals,
+                });
+                clubCollection.updateById(match.guestClub, {
+                    points: match.homeClub.points + 1,
+                    scoredGoals: match.guestClub.scoredGoals + guestGoals,
+                    receivedGoals: match.guestClub.receivedGoals + homeGoals,
+                });
+            }
+        });
+    });
+}
+
+function addGoals(match, homeGoals, guestGoals) {
+    var playerCollection = db.get("Player");
+    var allPlayers;
+    var playersInHomeClub = {};
+    var playersInGuestClub = {};
+
+    playerCollection.find().then(function (allPlayers) {
+
+        allPlayers.forEach(function (player) {
+            if (allPlayers.clubId === match.homeClub && homeGoals > 0) {
+                playersInHomeClub.push(player);
+            } else if (allPlayers.clubId === match.guestClub && guestGoals > 0) {
+                playersInGuestClub.push(player);
+            }
+        });
+
+        choosePlayerAndAddPlayerScore(match, true, homeGoals, playersInHomeClub);
+        choosePlayerAndAddPlayerScore(match, false, guestGoals, playersInGuestClub);
+    });
+
+}
+
+function choosePlayerAndAddPlayerScore(match, isPlayerInHomeClub, goals, playersInClub) {
+    for (goals; goals > 0; goals--) {
+        var playerNumberInArray = Math.floor(Math.random() * ((playersInClub.length - 1) - 0));
+        playersInClub[playerNumberInArray].scoredGoals += 1;
+
+        console.log("PlayerID: " + playersInClub[playerNumberInArray]._id);
+
+        if (isPlayerInHomeClub) {
+            match.homeGoals.push(playersInClub[playerNumberInArray]._id);
+        } else {
+            match.guestGoals.push(playersInClub[playerNumberInArray]._id);
+        }
+    }
+}
